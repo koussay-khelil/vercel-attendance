620acd21ec17fd0c547a3781ca0065c77a1216eb
koussay-khelil <khelil.koussay@gmail.com>
2023-03-02 13:35:25 +0100

    chore: update content

--- src/components/SignUpTable.js
+++ src/components/SignUpTable.js
@@ -738,7 +738,7 @@ const SignUpTable = (
                                     class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 "
                                     for="grid-organization"
                                   >
-                                    Associations
+                                    PME
                                   </label>
 
                                   <select
@@ -764,7 +764,7 @@ const SignUpTable = (
                                     class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 "
                                     for="grid-organization"
                                   >
-                                    Association
+                                    PME
                                   </label>
                                   <input
                                     autoFocus
